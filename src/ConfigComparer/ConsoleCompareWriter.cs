52c2957805e848473592a892e093e881e5b016f3
Tomas Jecha <tomas@jecha.net>
2023-10-18 12:22:31 +0200

    Add color output. Refactor.

---
+++ src/ConfigComparer/ConsoleCompareWriter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConfigComparer.CompareLineResult;
+
+namespace ConfigComparer
+{
+    public class ConsoleCompareWriter
+    {
+        public bool HideSame { get; set; } = false;
+        public bool HideValues { get; set; } = false;
+
+        public void Write(IEnumerable<CompareLineResult> compareLines)
+        {
+            foreach (var line in compareLines)
+            {
+                Write(line);
+            }
+        }
+
+        public void WriteStatsToConsole()
+        {
+            Console.WriteLine("Statistics:");
+            Console.WriteLine($" - Same items: {countSame}");
+            Console.WriteLine($" - Different items: {countDiff}");
+            Console.WriteLine($" - Left/right only: {countLeftOnly}/{countRightOnly}");
+        }
+
+        private int countSame, countDiff, countLeftOnly, countRightOnly;
+
+        public void Write(CompareLineResult line)
+        {
+            IncreaseStats(line);
+            WriteToConsole(line);
+        }
+
+        private void IncreaseStats(CompareLineResult line)
+        {
+            switch (line)
+            {
+                case Same:
+                    countSame++;
+                    break;
+                case Different:
+                    countDiff++;
+                    break;
+                case LeftOnly:
+                    countLeftOnly++;
+                    break;
+                case RightOnly:
+                    countRightOnly++;
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown type.");
+            }
+        }
+
+        private void WriteToConsole(CompareLineResult line)
+        {
+            var baseColor = Console.ForegroundColor;
+
+            const ConsoleColor colorAdd = ConsoleColor.Green;
+            const ConsoleColor colorRemove = ConsoleColor.Red;
+
+            switch (line)
+            {
+                case Same when HideSame:
+                    break;
+                case Same s:
+                    Console.Write($"    {s.Left.Key}");
+                    if (!HideValues)
+                    {
+                        Console.Write($" = {s.Left.Value}");
+                    }
+                    Console.WriteLine();
+                    break;
+                case Different d:
+                    Console.ForegroundColor = colorRemove;
+                    Console.Write($"-   {d.Left.Key}");
+                    if (!HideValues)
+                    {
+                        Console.Write($" = {d.Left.Value}");
+                    }
+                    Console.WriteLine();
+
+                    Console.ForegroundColor = colorAdd;
+                    Console.Write($"+   {d.Right.Key}");
+                    if (!HideValues)
+                    {
+                        Console.Write($" = {d.Right.Value}");
+                    }
+                    Console.WriteLine();
+                    break;
+                case LeftOnly l:
+                    Console.ForegroundColor = colorRemove;
+                    Console.Write($"-   {l.Left.Key}");
+                    if (!HideValues)
+                    {
+                        Console.Write($" = {l.Left.Value}");
+                    }
+                    Console.WriteLine();
+                    break;
+                case RightOnly r:
+                    Console.ForegroundColor = colorAdd;
+                    Console.Write($"+   {r.Right.Key}");
+                    if (!HideValues)
+                    {
+                        Console.Write($" = {r.Right.Value}");
+                    }
+                    Console.WriteLine();
+                    break;
+                default:
+                    throw new InvalidOperationException("Unknown type.");
+            }
+
+            Console.ForegroundColor = baseColor;
+        }
+    }
+}
--- src/ConfigComparer/Program.cs
+++ src/ConfigComparer/Program.cs
@@ -30,71 +30,22 @@ Parser.Default.ParseArguments<CompareOptions>(args)
         Console.WriteLine("Adding right configuration files.");
         var right = projector.Project(BuildSource(o.RightDirectory));
 
-        Console.WriteLine("Comparing.");
+        Console.WriteLine("---- diff start");
 
         var zipComparer = new ZipComparer(
             keyComparer: StringComparer.OrdinalIgnoreCase,
             valueComparer: StringComparer.Ordinal
         );
 
-        int same = 0;
-        int diff = 0;
-        int leftOnly = 0;
-        int rightOnly = 0;
-
-        foreach(var item in zipComparer.Compare(left, right))
+        var consoleWriter = new ConsoleCompareWriter()
         {
-            switch (item)
-            {
-                case CompareLineResult.Same s: 
-                    same++;
-                    if (!o.SkipSame)
-                    {
-                        Console.Write($"== [Same] {s.Left.Key} = {s.Left.Value}");
-                        if (!o.NoValues)
-                        {
-                            Console.Write($" = {s.Left.Value}");
-                        }
-                        Console.WriteLine();
-                    }
-                    break;
-                case CompareLineResult.Different d: 
-                    diff++; 
-                    Console.Write($"~~ [Different] {d.Left.Key}");
-                    if (!o.NoValues)
-                    {
-                        Console.Write($" = {d.Left.Value} / {d.Right.Value}");
-                    }
-                    Console.WriteLine();
-                    break;
-                case CompareLineResult.LeftOnly l: 
-                    leftOnly++; 
-                    Console.Write($"<< [LeftOnly] {l.Left.Key}");
-                    if (!o.NoValues)
-                    {
-                        Console.Write($" = {l.Left.Value}");
-                    }
-                    Console.WriteLine();
-                    break;
-                case CompareLineResult.RightOnly r: 
-                    rightOnly++; 
-                    Console.Write($">> [RightOnly] {r.Right.Key}");
-                    if (!o.NoValues)
-                    {
-                        Console.Write($" = {r.Right.Value}");
-                    }
-                    Console.WriteLine();
-                    break;
-                default:
-                    throw new InvalidOperationException("Unknown.");
-            }
-        }
+            HideSame = o.SkipSame,
+            HideValues = o.NoValues
+        };
 
-        Console.WriteLine("End.");
-        Console.WriteLine("Statistics:");
-        Console.WriteLine($" - Same items: {same}");
-        Console.WriteLine($" - Different items: {diff}");
-        Console.WriteLine($" - Left/right only: {leftOnly}/{rightOnly}");
+        var lines = zipComparer.Compare(left, right);
+        consoleWriter.Write(lines);
+        Console.WriteLine("---- diff end");
+        consoleWriter.WriteStatsToConsole();
         Console.WriteLine("Done.");
-
     });
